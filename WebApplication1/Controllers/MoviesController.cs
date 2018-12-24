4e9b93de2705cccdcbad68ab2debd31c73295d26
electronicsmf <electronicsmf@aol.com>
2018-12-24 08:59:14 -0500

    first commit

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+       
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Display(Name="Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name="Number in Stock")]
+        [Range(1,20)]
+        public byte NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
+    }
+}
